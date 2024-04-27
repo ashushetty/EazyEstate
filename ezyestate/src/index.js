27bd1e1f5dd7879c0502e2fa7196412e1928def9
ashish1 <ashushetty9121@gmail.com>
2024-04-27 20:06:21 +0530

    add redux persist

--- ezyestate/src/index.js
+++ ezyestate/src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import "./index.css";
-import {store} from './redux/store.js';
+import {persistor, store} from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
     <App />
    </BrowserRouter>
+   </PersistGate>
   </Provider>
 );
 
